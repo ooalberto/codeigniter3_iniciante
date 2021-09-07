b18d017e4a71fe290a9ef5f4446fc2cb44db1127
Alberto <oalberto@gmail.com>
2021-09-07 14:16:51 -0300

    subindo insert

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,24 +1,43 @@
-<?php 
+<?php
 
 
 class Pages extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('pages_model');
+	}
 	public function index()
 	{
 		#echo 'Retornando um controler dinanimo na rota Pages -: index()';
-		$this->load->model('pages_model');
+		#$this->load->model('pages_model');
 		$result = $this->pages_model->get();
-
-		$this->load->view('templates/header.php');
-		$this->load->view('pages/index.php',['pages'=>$result]);
-		$this->load->view('templates/footer.php');
-		
+		$this->load->view('templates/header');
+		$this->load->view('pages/index', ['pages' => $result]);
+		$this->load->view('templates/footer');
 	}
 
 	public function view($id = 'sem vl')
-		{
+	{
 
-			echo 'retornando uma view da View('.$id.')';
+		echo 'retornando uma view da View(' . $id . ')';
+	}
+
+	public function insert()
+	{
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('title', 'Título', 'required');
+		$this->form_validation->set_rules('body', 'Conteúdo', 'required');
+
+		if ($this->form_validation->run() === false) {
+			$this->load->view('templates/header');
+			$this->load->view('pages/insert');
+			$this->load->view('templates/footer');
+		} else {
+			$data['back'] = '/pages';
+			$this->pages_model->insert();
+			$this->load->view('templates/sucess', $data);
 		}
-	
+	}
 }
