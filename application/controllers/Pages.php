d10d9b33784c837b064ac1e9819b18296e2e55f9
Alberto <oalberto@gmail.com>
2021-09-08 09:00:41 -0300

    finalizando com 'auth usando o hook

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -18,10 +18,10 @@ class Pages extends CI_Controller
 		$this->load->view('templates/footer');
 	}
 
-	public function view($id = 'sem vl')
+	public function view($id)
 	{
-
-		echo 'retornando uma view da View(' . $id . ')';
+		$page = $this->pages_model->get($id);
+		$this->load->view('pages/view',['page'=>$page]);
 	}
 
 	public function insert()
@@ -37,7 +37,39 @@ class Pages extends CI_Controller
 		} else {
 			$data['back'] = '/pages';
 			$this->pages_model->insert();
-			$this->load->view('templates/sucess', $data);
+			$this->load->view('templates/success', $data);
 		}
+	}
+
+	public function edit($id = null)
+	{
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('title', 'Título', 'required');
+		$this->form_validation->set_rules('body', 'Conteúdo', 'required');
+
+		if ($this->form_validation->run() === false) {
+			$this->load->view('templates/header');
+			
+			$page = $this->pages_model->get($id);
+
+			$this->load->view('pages/edit', ['page'=>$page]);
+			$this->load->view('templates/footer');
+		} else {
+			$data['back'] = '/pages/' . $id;
+			$this->pages_model->update($id);
+			$this->load->view('templates/success', $data);
+		}
+	}
+
+	public function delete($id = null)
+	{
+		// $this->pages_model->delete($id);
+		// $this->load->view('templates/header');
+		// $this->load->view('/pages/');
+		// $this->load->view('templates/footer');
+
+		$data['back'] = '/pages';
+        $this->pages_model->delete($id);
+        $this->load->view('templates/success', $data);
 	}
 }
--- application/models/Pages_model.php
+++ application/models/Pages_model.php
@@ -2,16 +2,21 @@
 
 class Pages_model extends CI_Model
 {
+	private $table_name = 'pages';
+
 	public function __construct()
 	{
 		parent::__construct();
 		$this->load->database();
-		
 	}
-	public function get()
+	public function get($id = null)
 	{
-		$query = $this->db->get('pages');
-		return $query->result();
+		if ($id === null) {  #virificar o vl e o tp tem q ser null e n false 
+			$query = $this->db->get($this->table_name);
+			return $query->result();
+		}
+		$query = $this->db->get_where($this->table_name, ['id' => $id]);
+		return $query->first_row();
 	}
 
 	public function insert()
@@ -26,6 +31,27 @@ class Pages_model extends CI_Model
 			'slug'  => $slug
 		];
 
-		return $this->db->insert('pages', $data);
+		return $this->db->insert($this->table_name, $data);
+	}
+
+	public function update($id)
+	{
+		$this->load->helper('url');
+
+		$slug = url_title($this->input->post('title'), '_', true);
+
+		$data = [
+			'title' => $this->input->post('title'),
+			'body' => $this->input->post('body'),
+			'slug' => $slug
+		];
+
+		return $this->db->update($this->table_name, $data);
+	}
+
+	public function delete($id)
+	{
+	  return $this->db->delete($this->table_name, ['id' => $id]);
+ 
 	}
 }
